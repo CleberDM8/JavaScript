396895236db97339f8286da7e2cc0820b821f753
Cleber Dias <96442846+CleberDM8@users.noreply.github.com>
2024-02-06 17:21:06 -0300

    if, else, switch case

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`sua velocidade é de ${vel}km/h`)
+if(vel > 60){
+    console.log(`Voce foi MULTADO`)
+}
+console.log(`Dirija com cuidado`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "EUA"
+
+if(pais == "Brasil"){
+    console.log("Voce esta no Brasil!")
+} else{
+    console.log("Voce esta em outro País")
+}
