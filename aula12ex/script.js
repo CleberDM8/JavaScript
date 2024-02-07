b86ce700c6dbffc0205c2a6238c3f277b37c5114
Cleber Dias <96442846+CleberDM8@users.noreply.github.com>
2024-02-07 14:33:25 -0300

    consertando erros com img

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -5,19 +5,20 @@ function carregar(){
     var hora = data.getHours()
     
     
+    
 
     msg.innerHTML = `Agora sâo ${hora} horas.`
     if(hora >= 6 && hora < 12){
         //Bom Dia 
-        img.src = 'foto_manha.webp'
+        img.src = 'imagens/foto_manha.webp'
         document.body.style.background = '#e2cd9f'
     }else if(hora >= 12 && hora <= 18){
         // Boa Tarde
-        img.src = 'foto_tarde.webp'
+        img.src = 'imagens/foto_tarde.webp'
         document.body.style.background = '#b9846f'
     }else{
         //Boa noite
-        img.src = 'foto_noite.webp'
+        img.src = 'imagens/foto_noite.webp'
         document.body.style.background = '#515154'
     }
 
