7ec6d45cdaa2843d79988289ff60367fcda652bf
Cleber Dias <96442846+CleberDM8@users.noreply.github.com>
2024-02-23 14:48:38 -0300

    while/do while

---
+++ aula13/ex01.js
@@ -0,0 +1,18 @@
+var cont = 1 
+do{
+    console.log(`passo ${cont}`)
+    cont++
+}while(cont <= 5)
+
+    
+
+
+
+
+/*
+var cont = 1 
+while(cont <= 6){
+    console.log(`passo ${cont}`)
+    cont++
+}
+*/
