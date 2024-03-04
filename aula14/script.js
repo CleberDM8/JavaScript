9dc59ec513e572e78009a8bcc4f9c8ab974b5843
Cleber Dias <96442846+CleberDM8@users.noreply.github.com>
2024-03-04 16:02:12 -0300

    tabuada

--- aula14/script.js
+++ aula14/script.js
@@ -1,32 +1,21 @@
-function contar(){
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        //window.alert('[ERRO] Faltam dados!')
-        res.innerHTML = 'Impossível contar!'
-    } else {
-        res.innerHTML = 'Contando: <br> '
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if(p <= 0 ){
-            window.alert('Passo inválido! considerando PASSO 1')
-            p = 1
+    if(num.value.length == 0){
+        window.alert('Digite um numero!')
+    }else{
+        var n = Number(num.value)
+        var c = 1;
+        tab.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        if(i< f){
-            //contagem crescente
-            for(var c = i; c <= f; c += p){
-                res.innerHTML += `${c} \u{1F449}`
-            }
-        }else{
-            //contagem regressiva
-            for(var c = i; c >= f; c -= p){
-                res.innerHTML+=`${c} \u{1F449}`
-            }
-        }
-        res.innerHTML += `\u{1F3C1}`
     }
+
+    
 }
